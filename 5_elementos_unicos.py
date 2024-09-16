4bd9777f24e14e641dccd9543acc6e39f5acb480
vitavococo <gcastanedalondono@gmail.com>
2024-09-15 21:52:54 -0500

    Clase 5 con ejemplo de Breslev

--- 5_elementos_unicos.py
+++ 5_elementos_unicos.py
@@ -8,32 +8,37 @@ survey_responses = np.array(["bueno", "excelente", "malo",
                              "bueno", "bueno", "malo",
                              "excelente", "bueno", "malo", "malo", "HaShem ayudame con la ansiedad"])
 
-print("queremos ver los elementos que son únicos")
-print(np.unique(survey_responses))
+Breslev = np.array(["HaShem","HaShem", "Emunah", "Najman", "Ratzon", "simja", "simja", "Torah", "HaShem", "Emunah"])
 
-print("pero queremos saber cuales son los elementos únicos y contarlos")
-unique_elements, counts =np.unique(survey_responses, return_counts=True)
+print("Para ver los elementos únicos de ambos arrays")
+print(np.unique(survey_responses))
+print(np.unique(Breslev))
+print("queremos ver cuantas veces aparece un elemento")
+unique_elements, counts=np.unique(survey_responses, return_counts=True)
 print(unique_elements)
 print(counts)
+print("para el array de Breslev")
+elementos_unicos, counts=np.unique(Breslev, return_counts=True)
+print(elementos_unicos)
+print(counts)
 
-print("respecto a performance, hay que saber cuales elementos generan copias y cuales vistas")
+print("queremos ver que operaciones regresan copias y cuales vistas")
 
 array_x = np.arange(10)
-view_y= array_x[1:3]
+view_y=array_x[1:3]
 print(array_x)
 print(view_y)
-print("ahí cambiamos el valor de las posiciones 1 y3")
-print("Ahi el que opera sobre la raíz sufre un cambio")
+print("modificamos los valores de un elemento del array, entonces la vista también se verá modificada")
+print("Creamos copias para que no se modifique la raíz, cuando guardamos en nuevas variables")
+
 array_x[1:3] =[10, 11]
 print(array_x)
 print(view_y)
 
-print("vamos a trabajar con copias")
-
-array_x = np.arange(10)
 copy_x =array_x[[1,2]]
+print(array_x)
+print(view_y)
+array_x[1:3] =[22, 14]
+print(array_x)
+print("aquí no se realiza modificación en la raíz")
 print(copy_x)
-array_x[1:3]=[10,11]
-print(copy_x)
-
-print("los arrays son muy manipulables")
